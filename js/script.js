bff5f61127ab73fe54ba1df5e48d08353565af00
Larysa-tech <larysa.sydorchuk.uk@gmail.com>
2025-11-11 22:34:53 +0000

    First commit test

---
+++ js/script.js
@@ -0,0 +1,46 @@
+ "use strict"
+
+// const userName = "John";
+// let userNumber = 25;
+// userNumber = 24.5;
+
+// console.log(4 / 0);
+
+// const arrObj = {
+//     a: 1,
+//     d: 6
+// }
+
+// arrObj.b = "123";
+// // console.log(arrObj["b"]); or
+// const fob = "f";
+// arrObj.f = "1235";
+// console.log(arrObj.f);
+
+// const storeName = "Shop";
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ["Ali", "Anna", "Nargo"],
+//     products: {
+//         product1: 100,
+//         Product2: 200
+//     },
+//     open: true
+// }
+
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer);
+
+
+const answers = [];
+
+answers[0] = prompt("Вам есть 18?", "");
+answers[1] = prompt("Вам есть 40?", "");
+
+console.log([answers]);
+
+// document.write(answers);
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+const answers = [];
+
+answers[0] = prompt("Вам есть 18?", "");
+answers[1] = prompt("Вам есть 40?", "");
+
+console.log(answers);
